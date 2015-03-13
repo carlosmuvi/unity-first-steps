e789d66ef7ca8403568f748ed3c7f07cfd6c8182
kalospiles@gmail.com <kalospiles@gmail.com>
2015-03-13 16:25:13 +0100

    initial commit

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+
+	public int speed = 2;
+	int orientation = -1;
+	//TODO unused, use to detect when char is on air.
+	bool landed = false;
+	Rigidbody2D rigitbody;
+	Animator anim;
+
+	void Awake(){
+		rigitbody = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+		Flip ();
+	}
+
+	void FixedUpdate(){
+		Move ();
+	}
+
+	void Flip(){
+		orientation *= -1;
+		Vector3 theScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+		transform.localScale = theScale;
+	}
+
+	void Move(){
+		transform.position += transform.right * orientation * speed * Time.deltaTime;
+		//fix rotation
+		transform.rotation = Quaternion.identity;
+	}
+
+
+	void OnTriggerEnter2D(Collider2D other) {
+		string tag = other.gameObject.tag;
+		if(tag == "floor"){
+			landed = true;
+		}
+		else if(tag == "wall"){
+			Flip();
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if(other.gameObject.tag == "floor"){
+			landed = false;
+		}
+	}
+}
---
+++ Assets/Scripts/CharacterSpawn.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterSpawn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
